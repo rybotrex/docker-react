366bbd6fbb69fdab7a5832347115b1924912215f
krzys <nesa@vps723034.ovh.net>
2019-09-10 22:21:21 +0200

    text change

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Legia Warsauuuuuuu, auuuuuu
+          zmiana 333333 auu, auuuuuu
         </p>
       </div>
     );
